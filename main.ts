7bddc43136f8d887a929dfd13ccaf4fe80b5e19d
Kyle-Mathew-Magnaye8 <kyle.matthew.magnaye@stu.ocsb.ca>
2026-02-24 13:18:52 -0500

    updated code

--- main.ts
+++ main.ts
@@ -1,8 +1,25 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Kyle Matthew
+ * Created on: Feb 2026
+ * This program can roll a 6 sided dice.
 */
 
-basic.showString('Hello, World!')
+// our variables
+let currentTemperature: number
+let currentTemperatureIntoKelvin: number
+
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+basic.pause(1000)
+
+input.onButtonPressed(Button.A, function () {
+  // get light value
+  currentTemperature = input.temperature()
+  currentTemperatureIntoKelvin = (currentTemperature + 273.15)
+  currentTemperatureIntoKelvin = Math.round(currentTemperatureIntoKelvin)
+
+  // output answer
+  basic.clearScreen()
+  basic.showString('The temperature is: ' + currentTemperatureIntoKelvin.toString() + ' K')
+})
